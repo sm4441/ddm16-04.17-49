d3d242fa6e3ff3e446f2af192ea8076eadaff2db
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-05-06 16:33:09 -0300

    Create index.js

---
+++ src/Paginas/Detalhes/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native-web";
+
+export default function Detalhes(){
+    return(
+        <View>
+            <Text> ESSA É A TELA DE DETALHES</Text>
+        </View>
+    )
+}
